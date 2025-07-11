9e4c6e4f6473daf020b7901ca5e45cc0f1b9d21a
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-07-11 15:00:55 +0530

    Time: 2 ms (67.97%) | Memory: 43.2 MB (8.70%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,60 +1,58 @@
 class Pair{
-    int row;
-    int col;
+    int first;
+    int second;
     int time;
-    Pair(int r,int c,int t){
-        this.row=r;
-        this.col=c;
-        this.time=t;
+    Pair(int first,int second,int time){
+        this.first=first;
+        this.second=second;
+        this.time=time;
+
+
     }
 }
 class Solution {
     public int orangesRotting(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        int visited[][]=new int[m][n];
-        Queue<Pair>q=new LinkedList<>();
-        int freshc=0;
+        int freshcount=0;
+        int count=0;
+        int time=0;
+        Queue<Pair>q= new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
                     q.add(new Pair(i,j,0));
-                    visited[i][j]=2;
-                }
-                else{
-                    visited[i][j]=0;
                 }
                 if(grid[i][j]==1){
-                    freshc++;
+                    freshcount++;
+
                 }
             }
         }
-        int count=0;
-        int time=0;
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
-
         while(!q.isEmpty()){
             Pair val=q.remove();
-            int r=val.row;
-            int c=val.col;
+            int row=val.first;
+            int col=val.second;
             int t=val.time;
             time=Math.max(time,t);
-            for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(0<=nrow&&nrow<m&&0<=ncol&&ncol<n&&visited[nrow][ncol]==0&&grid[nrow][ncol]==1){
+            int drow[]={-1,0,1,0};
+            int dcol[]={0,1,0,-1};
+            for(int k=0;k<4;k++){
+                int nrow=row+drow[k];
+                int ncol=col+dcol[k];
+                if(nrow>=0&&nrow<m&&ncol>=0&&ncol<n&&grid[nrow][ncol]==1){
+                    grid[nrow][ncol]=2;
                     q.add(new Pair(nrow,ncol,t+1));
-                    visited[nrow][ncol]=2;
                     count++;
                 }
             }
+
         }
-        if(count!=freshc){
+        if(count!=freshcount){
             return -1;
         }
-        return time;
 
-        
+
+    return time;    
     }
 }
