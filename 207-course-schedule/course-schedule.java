02a164ba3037ef416835f695d0be429f16fa4048
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-07-11 20:31:21 +0530

    Time: 3 ms (99.90%) | Memory: 45.2 MB (76.68%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,56 +1,53 @@
 class Edge{
     int src;
     int dest;
-    Edge(int s, int d){
-        this.src=s;
-        this.dest=d;
+    Edge(int src,int dest){
+        this.src=src;
+        this.dest=dest;
     }
 }
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         ArrayList<Edge>graph[]=new ArrayList[numCourses];
-        boolean val=false;
-        boolean visited[]= new boolean[numCourses];
-         boolean rec[]= new boolean[numCourses];
         for(int i=0;i<numCourses;i++){
             graph[i]=new ArrayList<Edge>();
         }
-        
-        for(int i=0;i< prerequisites.length;i++){
-            int dest= prerequisites[i][0];
-            int src= prerequisites[i][1];
-            graph[src].add(new Edge(src,dest));
-
+        for(int i=0;i<prerequisites.length;i++){
+            int u=prerequisites[i][1];
+            int v=prerequisites[i][0];
+            graph[u].add(new Edge(u,v));
         }
-        for(int i=0;i<numCourses;i++){
-            if(visited[i]==false){
-                 val=dfs(graph,visited,rec,i);
+        boolean vis[]= new boolean[numCourses];
+        boolean rec[]= new boolean[numCourses];
+        for(int i=0;i<vis.length;i++){
+            if(vis[i]==false){
+                boolean val=dfs(graph,i,vis,rec);
                 if(val){
-                    break;
+                    return !val;
+                    
                 }
-
             }
-
         }
-        return !val;
+        return true;
         
     }
-    public boolean dfs(ArrayList<Edge>graph[],boolean visited[],boolean rec[],int curr){
-        visited[curr]=true;
-         rec[curr]=true;
-         for(int i=0;i<graph[curr].size();i++){
-            Edge e=graph[curr].get(i);
-            if(!visited[e.dest]){
-                if(dfs(graph,visited,rec,e.dest)){
+    public boolean dfs(ArrayList<Edge>graph[],int curr,boolean vis[],boolean rec[]){
+        vis[curr]=true;
+        rec[curr]=true;
+        for(int i=0;i<graph[curr].size();i++){
+            Edge e= graph[curr].get(i);
+            if(rec[e.dest]==true){
+                return true;
+            }
+            if(vis[e.dest]==false){
+                if(dfs(graph,e.dest,vis,rec)){
                     return true;
                 }
             }
-            else if(rec[e.dest]==true){
-                return true;
-            }
-         }
-         rec[curr]=false;
-         return false;
+        }
+        rec[curr]=false;
+        return false;
+
 
     }
 }
