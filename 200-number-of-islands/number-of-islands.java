c7b4680680d199f3fb7273c9fd6572936f9ba604
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-07-11 12:43:56 +0530

    Time: 2 ms (99.81%) | Memory: 49.9 MB (40.55%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,42 +1,29 @@
-class Pair{
-    int row;
-    int col;
-    Pair(int r, int c){
-        this.row=r;
-        this.col=c;
-    }
-}
 class Solution {
     public int numIslands(char[][] grid) {
+        int row=grid.length;
+        int col=grid[0].length;
         int count=0;
-        int m=grid.length;
-        int n=grid[0].length;
-        Queue<Pair>q=new LinkedList<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
                 if(grid[i][j]=='1'){
-                    q.add(new Pair(i,j));
+                    dfs(grid,i,j);
                     count++;
-                    grid[i][j]=0;
-                }
-                int[] drow = {-1, 0, 1, 0};
-                int[] dcol = {0, 1, 0, -1};
-                while(!q.isEmpty()){
-                    Pair val=q.remove();
-                    int row=val.row;
-                    int col=val.col;
-                    for(int k=0;k<4;k++){
-                        int nrow=row+drow[k];
-                        int ncol=col+dcol[k];
-                        if(0<=nrow&&nrow<m&&0<=ncol&&ncol<n&&grid[nrow][ncol]=='1'){
-                            q.add(new Pair(nrow,ncol));
-                            grid[nrow][ncol]='0';
-                        }
-                    }
                 }
             }
         }
         return count;
         
+    }
+    public void dfs(char grid[][],int i,int j){
+        int row=grid.length;
+        int col=grid[0].length;
+        if(i<0||j<0||i>=row||j>=col||grid[i][j]=='0'){
+            return;
+        }
+        grid[i][j]='0';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
     }
 }
