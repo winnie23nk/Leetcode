988fc436fd972668350f362a337f6bf15385d788
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-07-11 13:12:25 +0530

    Time: 4 ms (51.23%) | Memory: 52.3 MB (17.32%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,3 +1,11 @@
+class Pair{
+    int src;
+    int dest;
+    Pair(int src,int dest){
+        this.src=src;
+        this.dest=dest;
+    }
+}
 class Solution {
     public int numIslands(char[][] grid) {
         int row=grid.length;
@@ -6,7 +14,7 @@ class Solution {
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j]=='1'){
-                    dfs(grid,i,j);
+                    bfs(grid,i,j);
                     count++;
                 }
             }
@@ -14,16 +22,27 @@ class Solution {
         return count;
         
     }
-    public void dfs(char grid[][],int i,int j){
-        int row=grid.length;
-        int col=grid[0].length;
-        if(i<0||j<0||i>=row||j>=col||grid[i][j]=='0'){
-            return;
-        }
+    public void bfs(char grid[][],int i,int j){
         grid[i][j]='0';
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+        int m=grid.length;
+        int n=grid[0].length;
+        Queue<Pair>q=new LinkedList<>();
+        q.add(new Pair(i,j));
+        while(!q.isEmpty()){
+            Pair val=q.remove();
+            int row=val.src;
+            int col=val.dest;
+            int drow[]={-1,0,1,0};
+            int dcol[]={0,1,0,-1};
+            for(int k=0;k<4;k++){
+                int nrow=row+drow[k];
+                int ncol=col+dcol[k];
+                if(nrow>=0&&nrow<m&&ncol>=0&&ncol<n&&grid[nrow][ncol]=='1'){
+                    q.add(new Pair(nrow,ncol));
+                    grid[nrow][ncol]='0';
+                }
+            }
+        }
+
     }
 }
