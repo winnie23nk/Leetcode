0750ef223d30efe1dff1119021648072a5f34650
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-07-12 19:34:30 +0530

    Time: 4 ms (92.29%) | Memory: 45.6 MB (92.55%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -8,73 +8,40 @@ class Edge{
 }
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-         ArrayList<Edge>graph[]=new ArrayList[numCourses];
-         int order[]= new int [numCourses];
-         int empty[]={};
+        ArrayList<Edge>graph[]= new ArrayList[numCourses];
+        int inorder[]= new int[numCourses];
+         int order[]= new int[numCourses];
+         int arr[]={};
+         int j=0;
+        Queue<Integer>q=new LinkedList<>();
         for(int i=0;i<numCourses;i++){
-            graph[i]=new ArrayList<Edge>();
+            graph[i]= new ArrayList<Edge>();
         }
         for(int i=0;i<prerequisites.length;i++){
-            int u=prerequisites[i][1];
+            int u= prerequisites[i][1];
             int v=prerequisites[i][0];
             graph[u].add(new Edge(u,v));
+            inorder[v]++;
         }
-        boolean vis[]= new boolean[numCourses];
-        boolean visited[]=new boolean[numCourses];
-        Stack<Integer>s=new Stack<>();
-        boolean rec[]= new boolean[numCourses];
-       for(int i=0;i<visited.length;i++){
-        if(visited[i]==false){
-            boolean val=cycle(graph,i,visited,rec);
-            if(val){
-                return empty;
+        for(int i=0;i<numCourses;i++){
+            if(inorder[i]==0){
+                q.add(i);
             }
         }
-       }
-       for(int i=0;i<vis.length;i++){
-        if(vis[i]==false){
-            dfs(graph,i,vis,s);
-        }
-       }
-        int i=0;
-        while(!s.isEmpty()){
-           
-                order[i++]=s.pop();
-           
 
-        }
-        return order;
-        
-    }
-
-    public void dfs(ArrayList<Edge>graph[],int curr, boolean vis[],Stack<Integer>s){
-        vis[curr]=true;
-        for(int i=0;i<graph[curr].size();i++){
-            Edge e= graph[curr].get(i);
-            if(vis[e.dest]==false){
-                dfs(graph,e.dest,vis,s);
+        while(!q.isEmpty()){
+            int curr=q.remove();
+            order[j++]=curr;
+           for(int i=0;i<graph[curr].size();i++){
+            Edge e=graph[curr].get(i);
+            inorder[e.dest]--;
+            if(inorder[e.dest]==0){
+                q.add(e.dest);
             }
+           }
         }
-        s.push(curr);
-    }
-
-    public boolean cycle(ArrayList<Edge>graph[],int curr,boolean vis[],boolean rec[]){
-        vis[curr]=true;
-        rec[curr]=true;
-        for(int i=0;i<graph[curr].size();i++){
-            Edge e= graph[curr].get(i);
-            if(rec[e.dest]==true){
-                return true;
-            }
-            if(vis[e.dest]==false){
-                if(cycle(graph,e.dest,vis,rec)){
-                    return true;
-                }
-            }
-        }
-        rec[curr]=false;
-        return false;
 
 
+        return j==numCourses?order:arr;
     }
 }
