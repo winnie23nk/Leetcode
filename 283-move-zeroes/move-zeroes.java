323b4335a8833d197e0e6ba99774a39ecb8368b6
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-07-15 17:11:39 +0530

    Time: 2 ms (80.43%) | Memory: 46.3 MB (39.06%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,23 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        ArrayList<Integer>arr=new ArrayList<>();
-        int count=0;
-        if(nums.length==1){
-            return;
-        }
+        int j=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                count++;
-            }
-            else{
-                arr.add(nums[i]);
+            if(nums[i]!=0){
+                nums[j++]=nums[i];               
             }
         }
-        for(int i=0;i<arr.size();i++){
-            nums[i]=arr.get(i);
-        }
-        for(int i=0;i<count;i++){
-            nums[arr.size()+i]=0;
+        while(j!=nums.length){
+            nums[j++]=0;
         }
         
     }
