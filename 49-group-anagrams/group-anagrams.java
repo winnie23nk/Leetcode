b3f7010a10d8b2f8dd63e868bd1959162f536220
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-07-16 19:29:04 +0530

    Time: 14 ms (29.57%) | Memory: 48.2 MB (28.46%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,18 +1,30 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,ArrayList<String>>map= new HashMap<>();
+        HashMap<String,ArrayList<String>>map=new HashMap<>();
         for(int i=0;i<strs.length;i++){
-            char arr[]=strs[i].toCharArray();
-            Arrays.sort(arr);
-            String val=new String(arr);
+            String val=frequencyString(strs[i]);
             if(!map.containsKey(val)){
-                ArrayList<String>list= new ArrayList<>();
-                map.put(val,list);
+                map.put(val,new ArrayList<>());
             }
-                map.get(val).add(strs[i]);
-            
+            map.get(val).add(strs[i]);
         }
         return new ArrayList<>(map.values());
         
+    }
+    public String frequencyString(String str){
+        int freq[]= new int[26];
+        char arr[]= str.toCharArray();
+        for(int i=0;i<arr.length;i++){
+            int idx=arr[i]-'a';
+            freq[idx]++;
+        }
+        StringBuilder sb= new StringBuilder();
+        for(int i=0;i<freq.length;i++){
+            char c='a';
+            sb.append(c);
+            sb.append(freq[i]);
+            c++;
+        }
+        return sb.toString();
     }
 }
