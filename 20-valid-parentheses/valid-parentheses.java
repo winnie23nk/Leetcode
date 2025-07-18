63eff6a158a7fdf1af0be992d25ea76bc4ccc2e7
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-07-18 21:02:56 +0530

    Time: 4 ms (11.71%) | Memory: 42 MB (31.39%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,18 +1,49 @@
 class Solution {
     public boolean isValid(String s) {
+        if(s.length()%2!=0){
+            return false;
+        }
         Stack<Character>st=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(')
-             st.push(')');
-            else if(s.charAt(i)=='{')
-             st.push('}');
-            else if(s.charAt(i)=='[')
-             st.push(']');
-           else if(st.isEmpty()||st.peek()!=s.charAt(i))
-           return false;
-           else
-           st.pop();
+        int i=0;
+        while(i<s.length()){
+            if(s.charAt(i)=='('||s.charAt(i)=='['||s.charAt(i)=='{'){
+                st.push(s.charAt(i));
+            }
+           if(s.charAt(i)==')'){
+            if(st.isEmpty()){
+                return false;
+            }
+            char val=st.pop();
+            if(val!='('){
+                return false;
+            }
+           }
+
+           if(s.charAt(i)==']'){
+            if(st.isEmpty()){
+                return false;
+            }
+            char val=st.pop();
+            if(val!='['){
+                return false;
+            }
+           }
+
+           if(s.charAt(i)=='}'){
+            if(st.isEmpty()){
+                return false;
+            }
+            char val=st.pop();
+            if(val!='{'){
+                return false;
+            }
+           }
+           i++;
+        }
+        if(st.isEmpty()){
+            return true;
+        }
+        return false;
+        
     }
-    return st.isEmpty();
-}
 }
