bbfefdf57f9168a02a8facf41b05e7c1efc4fbc3
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-07-18 19:52:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (36.73%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,22 +11,27 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
+        if(head==null||head.next==null){
+            return null;
+        }
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null&&fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+             slow=slow.next;
+             fast=fast.next.next;
             if(slow==fast){
-                break;
-            }
-        }
-        if(fast==null||fast.next==null){
-            return null;
-        }
-        while(head!=slow){
-            head=head.next;
+                 slow=head;
+        while(slow!=fast){
             slow=slow.next;
+            fast=fast.next;
         }
-        return head;
+        return slow;
+       
+            }
+           
+        }
+        
+        return null;
+        
     }
 }
