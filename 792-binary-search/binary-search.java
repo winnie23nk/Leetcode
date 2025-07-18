97050e0168c793c2519ab74fa25b66056a9883c4
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-07-18 14:28:31 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (60.39%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,20 +1,23 @@
 class Solution {
-    public int search(int[] nums, int target) {
-        int left=0;
-        int right=nums.length-1;
-        while(left<=right){
-            int mid=(left+right)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            if(nums[mid]<target){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
-            }
+    public static int binsearch(int arr[],int k,int low,int high){
+        if(low>high){
+            return -1;
         }
-        return -1;
-      
+        
+        int mid=low+(high-low)/2;
+        if(k==arr[mid]){
+            return mid;
+        }
+        if(k<arr[mid]){
+            return binsearch(arr,k,low,mid-1);
+        }
+        
+            return binsearch(arr,k,mid+1,high);
+
+    }
+    public int search(int[] nums, int target) {
+        int val=binsearch(nums,target,0,nums.length-1);
+        return val;
+        
     }
 }
