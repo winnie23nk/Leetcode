edf9ef6992d218622615116f54e04dbbcb2a89a3
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-07-28 18:45:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (65.56%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,24 +1,21 @@
 class Solution {
     public int climbStairs(int n) {
-        int sum=0;
-        int mem[]=new int[n+1];
-        Arrays.fill(mem,-1);
-        return dp(n,sum,mem);
+        int sum=n;
+         int mem[]=new int[n+1];
+         Arrays.fill(mem,-1);
+
+        return dp(sum,n,mem);
         
     }
-    public int dp(int n,int sum,int mem[]){
-        if(sum==n){
-            return 1;
-        }
-        if(sum>n){
-            return 0;
+    public int dp(int sum,int n,int[]mem){
+        if(sum<=2){
+            return sum;
         }
         if(mem[sum]!=-1){
             return mem[sum];
         }
-        
-        int left=dp(n,sum+1,mem);
-        int right=dp(n,sum+2,mem);
+        int left=dp(sum-1,n,mem);
+        int right=dp(sum-2,n,mem);
         return mem[sum]=left+right;
     }
 }
