dbf9e618b4a7b391bb2ef7cc64031197e43a9517
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-08-08 13:56:37 +0530

    Time: 5 ms (27.10%) | Memory: 47.6 MB (68.29%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -2,30 +2,31 @@ class Solution {
     public int minPathSum(int[][] grid) {
         int m= grid.length;
         int n=grid[0].length;
-        int mem[][]= new int[m][n];
+        int mem[]= new int[n];
+        int curr[]= new int[n];
         for(int i=0;i<m;i++){
+            curr= new int[n];
             for(int j=0;j<n;j++){
-                mem[i][j]=-1;
+                if(i==0&&j==0){
+                    curr[j]=grid[i][j];
+                }
+                else{
+                int top=Integer.MAX_VALUE;
+                int left=Integer.MAX_VALUE;
+                if(i!=0){
+                top=mem[j]+grid[i][j];
+                }
+                if(j!=0){
+                left=curr[j-1]+grid[i][j];
+                }
+                curr[j]=Math.min(top,left);
+                }
+
+
             }
+            mem=curr;
         }
-        return dp(m-1,n-1,grid,mem);
-        
-    }
-    public int dp(int m, int n, int[][]grid,int mem[][]){
-        if(m==0&&n==0){
-            return grid[m][n];
-        }
-        if(m<0){
-            return Integer.MAX_VALUE-200;
-        }
-        if(n<0){
-             return Integer.MAX_VALUE-200;
-        }
-        if(mem[m][n]!=-1){
-            return mem[m][n];
-        }
-        int top=grid[m][n]+dp(m-1,n,grid,mem);
-        int left=grid[m][n]+dp(m,n-1,grid,mem);
-        return mem[m][n]=Math.min(top,left);
+        return curr[n-1];
+      
     }
 }
