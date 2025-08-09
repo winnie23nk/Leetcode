e4a982da3c7f0280d7890ceee2abd3c965d16395
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-08-09 19:15:47 +0530

    Time: 10 ms (67.63%) | Memory: 44.9 MB (48.80%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -3,26 +3,18 @@ class Solution {
         int l=0;
         int r=0;
         int maxlength=0;
-        int arr[]= new int [26];
         int maxfreq=0;
+        int arr[]= new int[26];
         while(r<s.length()){
-            char c=s.charAt(r);
-            arr[c-'A']++;
-            maxfreq=Math.max(maxfreq,arr[c-'A']);
-            if((r-l+1)-maxfreq<=k){
-                maxlength=Math.max(maxlength,r-l+1);
-            }else{
-                while(r-l+1-maxfreq>k){
-                    arr[s.charAt(l)-'A']--;
-                    l=l+1;
-                    maxfreq=0;
-                    for(int i=0;i<26;i++){
-                        maxfreq=Math.max(maxfreq,arr[i]);
-                    }
-                }
-            }
-            r++;
-
+         char c= s.charAt(r);
+         arr[c-'A']++;
+         maxfreq=Math.max(maxfreq,arr[c-'A']);
+         if((r-l+1)-maxfreq>k){
+            arr[s.charAt(l)-'A']--;
+            l++;
+         }
+         maxlength=Math.max(maxlength,r-l+1);
+         r++;
         }
         return maxlength;
         
