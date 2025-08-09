b2a66ad8b5aa99cfb945a49fdb7f56d29de111cb
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-08-09 13:43:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (32.17%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -8,19 +8,12 @@ class Solution {
         }
         String st=strs[0];
         for(int i=1;i<strs.length;i++){
-            String st1=strs[i];
-             int count=0;
-            for(int j=0;j<Math.min(st.length(),st1.length());j++){
-                if(st.charAt(j)==st1.charAt(j)){
-                    count++;
-                }else{
-                    break;
-                }
-            }
-             if(count==0){
+            while(strs[i].indexOf(st)!=0){
+                st=st.substring(0,st.length()-1);
+                if(st.length()==0){
                     return "";
                 }
-                st=st.substring(0,count);
+            }
         }
         return st;
         
