0433636e23891528229c13436502836a5167a60a
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-08-11 17:03:50 +0530

    Time: 22 ms (9.13%) | Memory: 44.6 MB (50.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,32 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char a[]= s.toCharArray();
-        char b[]= t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        if(Arrays.equals(a,b)){
+        if(s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+            }
+            else{
+                map.put(s.charAt(i),1);
+            }
+        }
+        for(int j=0;j<s.length();j++){
+            if(map.containsKey(t.charAt(j))){
+                if(map.get(t.charAt(j))==1){
+                    map.remove(t.charAt(j));
+                }
+                else{
+                     map.put(t.charAt(j),map.get(t.charAt(j))-1);
+                }
+            }
+
+        }
+        if(map.isEmpty()){
             return true;
         }
-
-       return false; 
+        return false;
+        
     }
 }
