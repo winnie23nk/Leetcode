57190ed38dd92924df40c77e1fe1821047de3f70
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-08-18 19:06:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (6.29%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,30 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low=0;
-        int high=nums.length-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                return mid;
-            }
-            if(nums[mid]>=nums[low]){
-                if(nums[low]<=target&&target<nums[mid]) {
-                    high=mid-1;
-                }else{
-                    low=mid+1;
-                }      
-            }
-            else{
-                if(nums[mid]<=target&&target<=nums[high]){
-                low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
-
-            }
+        return bin(nums,target,0,nums.length-1);
+        
+    }
+    public int bin(int[]nums,int target, int beg,int end){
+        if(beg>end){
+            return -1;
         }
-        return -1;
+        int mid=beg+(end-beg)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        if(mid!=nums.length-1&&nums[mid+1]<=target&&target<=nums[end]){
+            return bin(nums,target,mid+1,end);
+        }else{
+            int val= bin(nums,target,beg,mid-1);
+            int val2=bin(nums,target,mid+1,end);
+            return Math.max(val,val2);
+        }
+
+         
+        
+        
         
     }
 }
