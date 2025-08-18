fbf2b93051c60577715be0462a7ddf7a2a19c294
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-08-18 19:16:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (17.67%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,37 +1,26 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-         int low=0;
-        int high=nums.length-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target){
-                return true;
-            }
-            if(nums[low]==nums[mid]&&nums[mid]==nums[high]){
-                low=low+1;
-                high=high-1;
-                continue;
-
-            }
-            if(nums[mid]>=nums[low]){
-                if(nums[low]<=target&&target<nums[mid]) {
-                    high=mid-1;
-                }else{
-                    low=mid+1;
-                }      
-            }
-            else{
-                if(nums[mid]<=target&&target<=nums[high]){
-                low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
-
-            }
-        }
-        return false;
-        
-        
+        return bin(nums,target,0,nums.length-1);
     }
+    public boolean bin(int[]nums,int target, int beg,int end){
+        if(beg>end){
+            return false;
+        }
+        int mid=beg+(end-beg)/2;
+        if(nums[mid]==target){
+            return true;
+        }
+        if(mid!=nums.length-1&&nums[mid+1]<=target&&target<=nums[end]){
+            return bin(nums,target,mid+1,end);
+        }else{
+            boolean val= bin(nums,target,beg,mid-1);
+            
+            if(val==false){
+             boolean val2=bin(nums,target,mid+1,end);
+             return val2;
+            }
+            return val;
+        }
+        
+}
 }
