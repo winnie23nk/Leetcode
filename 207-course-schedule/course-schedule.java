ebadb1e7e48f2ecb61e6fa0b57aebaa9b4fe1bea
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-09-13 14:11:59 +0530

    Time: 7 ms (44.10%) | Memory: 45.4 MB (53.08%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,53 +1,37 @@
-class Edge{
-    int src;
-    int dest;
-    Edge(int src,int dest){
-        this.src=src;
-        this.dest=dest;
-    }
-}
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<Edge>graph[]=new ArrayList[numCourses];
+        ArrayList<ArrayList<Integer>>list=new ArrayList<>();
+        ArrayList<Integer>arr=new ArrayList<>();
+        Queue<Integer>q= new LinkedList<>();
         for(int i=0;i<numCourses;i++){
-            graph[i]=new ArrayList<Edge>();
+            list.add(new ArrayList<>());
         }
+        int indeg[]= new int[numCourses];
         for(int i=0;i<prerequisites.length;i++){
-            int u=prerequisites[i][1];
-            int v=prerequisites[i][0];
-            graph[u].add(new Edge(u,v));
+            indeg[prerequisites[i][1]]++;
+            list.get(prerequisites[i][0]).add(prerequisites[i][1]);
         }
-        boolean vis[]= new boolean[numCourses];
-        boolean rec[]= new boolean[numCourses];
-        for(int i=0;i<vis.length;i++){
-            if(vis[i]==false){
-                boolean val=dfs(graph,i,vis,rec);
-                if(val){
-                    return !val;
-                    
-                }
+        for(int i=0;i<numCourses;i++){
+            if(indeg[i]==0){
+                q.add(i);
             }
         }
-        return true;
+        topo(q,list,indeg,arr);
+       return arr.size()==numCourses?true:false; 
         
     }
-    public boolean dfs(ArrayList<Edge>graph[],int curr,boolean vis[],boolean rec[]){
-        vis[curr]=true;
-        rec[curr]=true;
-        for(int i=0;i<graph[curr].size();i++){
-            Edge e= graph[curr].get(i);
-            if(rec[e.dest]==true){
-                return true;
-            }
-            if(vis[e.dest]==false){
-                if(dfs(graph,e.dest,vis,rec)){
-                    return true;
+    public void topo(Queue<Integer>q,ArrayList<ArrayList<Integer>>list,int[]indeg,ArrayList<Integer>arr){
+       
+        while(!q.isEmpty()){
+            int val=q.remove();
+            arr.add(val);
+            for(int i=0;i<list.get(val).size();i++){
+                int ele=list.get(val).get(i);
+                indeg[ele]--;
+                if(indeg[ele]==0){
+                    q.add(ele);
                 }
             }
         }
-        rec[curr]=false;
-        return false;
-
-
     }
 }
