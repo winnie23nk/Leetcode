734ecf07704d7434796a13bd3e000007eb9c4d93
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-09-13 20:46:49 +0530

    Time: 24 ms (40.08%) | Memory: 55.8 MB (18.33%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,37 +1,39 @@
+import java.util.*;
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int visited[]=new int[graph.length];
-        int pvisited[]=new int[graph.length];
-        int safe[]=new int[graph.length];
-
+        int outdeg[]= new int[graph.length];
+        Queue<Integer>q=new LinkedList<>();
+        ArrayList<Integer>sol=new ArrayList<>();
         for(int i=0;i<graph.length;i++){
-            if(visited[i]==0){
-                dfs(visited,pvisited,safe,graph,i);
+            outdeg[i]=graph[i].length;
+        }
+        ArrayList<ArrayList<Integer>>list=new ArrayList<>();
+        for(int i=0;i<graph.length;i++){
+            list.add(new ArrayList<>());
+        }
+        for(int i=graph.length-1;i>=0;i--){
+            for(int j=graph[i].length-1;j>=0;j--){
+                list.get(graph[i][j]).add(i);
             }
         }
+        for(int i=graph.length-1;i>=0;i--){
+            if(outdeg[i]==0){
+                q.add(i);
+            }
+        }
+            while(!q.isEmpty()){
+                int val=q.remove();
+                sol.add(val);
+                for(int i=0;i<list.get(val).size();i++){
+                    int val2=list.get(val).get(i);
+                    outdeg[val2]--;
+                    if(outdeg[val2]==0){
+                        q.add(val2);
+                    }
+                }
+            }
         
-        ArrayList<Integer>list=new ArrayList<>();
-        for(int i=0;i<safe.length;i++){
-            if(safe[i]==1){
-                list.add(i);
-            }
-        }
-        return list;
+        Collections.sort(sol);
+        return sol;
     }
-    public boolean dfs(int[] vis,int[] pvis,int[] safe,int[][]graph,int start){
-        vis[start]=1;
-        pvis[start]=1;
-        for(int i=0;i<graph[start].length;i++){
-            int val=graph[start][i];
-            if(vis[val]==0){
-                if(!dfs(vis,pvis,safe,graph,val))return false;
-            }
-            if(pvis[val]==1){
-                return false;
-            }
-    }
-        pvis[start]=0;
-        safe[start]=1;
-        return true;
-}
 }
