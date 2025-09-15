6f9acebe4f269a64415631cbcda43d556c95d2a9
Tejaswini N K <153833481+winnie23nk@users.noreply.github.com>
2025-09-15 19:56:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (18.19%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,26 +1,34 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        return bin(nums,target,0,nums.length-1);
+    boolean val=find(nums,0,nums.length-1,target);
+    return val;
     }
-    public boolean bin(int[]nums,int target, int beg,int end){
-        if(beg>end){
-            return false;
-        }
-        int mid=beg+(end-beg)/2;
+    public boolean find(int nums[],int start,int end,int target){
+        
+        while(start<=end){
+        int mid=start+(end-start)/2;
         if(nums[mid]==target){
             return true;
         }
-        if(mid!=nums.length-1&&nums[mid+1]<=target&&target<=nums[end]){
-            return bin(nums,target,mid+1,end);
-        }else{
-            boolean val= bin(nums,target,beg,mid-1);
-            
-            if(val==false){
-             boolean val2=bin(nums,target,mid+1,end);
-             return val2;
-            }
-            return val;
+        if(nums[start]==nums[mid]&&nums[mid]==nums[end]){
+            start=start+1;
+            end=end-1;
         }
-        
+        else if(nums[start]<=nums[mid]){
+            if(nums[start]<=target&&target<=nums[mid]){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }else{
+            if(nums[mid]<=target&&target<=nums[end]){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+       
+    }
+    return false;
 }
 }
